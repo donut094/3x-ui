fea0288406680dffa82c19c6500b8a47089c9477
Donut Studio <72498893+donut094@users.noreply.github.com>
2024-03-18 23:02:30 +0700

    Add files via upload

---
+++ web/assets/js/langs.js
@@ -0,0 +1,53 @@
+const supportLangs = [
+    {
+        name: 'English',
+        value: 'en-US',
+        icon: '🇺🇸',
+    },
+    {
+        name: 'ไทย',
+        value: 'th-TH',
+        icon: '🇹🇭',
+    },
+];
+
+function getLang() {
+    let lang = getCookie('lang');
+
+    if (!lang) {
+        if (window.navigator) {
+            lang = window.navigator.language || window.navigator.userLanguage;
+
+            if (isSupportLang(lang)) {
+                setCookie('lang', lang, 150);
+            } else {
+                setCookie('lang', 'en-US', 150);
+                window.location.reload();
+            }
+        } else {
+            setCookie('lang', 'en-US', 150);
+            window.location.reload();
+        }
+    }
+
+    return lang;
+}
+
+function setLang(lang) {
+    if (!isSupportLang(lang)) {
+        lang = 'en-US';
+    }
+
+    setCookie('lang', lang, 150);
+    window.location.reload();
+}
+
+function isSupportLang(lang) {
+    for (l of supportLangs) {
+        if (l.value === lang) {
+            return true;
+        }
+    }
+
+    return false;
+}
